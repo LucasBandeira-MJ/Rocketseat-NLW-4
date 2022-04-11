8a83ed5c6b27b8c769d896c55d0379d7b9803818
Lucas Bandeira <lucasbandeira.mj@outlook.com>
2022-04-11 14:44:20 -0300

    Added countdown start button to Countdown component and styled it

--- src/components/Countdown/index.tsx
+++ src/components/Countdown/index.tsx
@@ -2,16 +2,22 @@ import styles from './styles.module.sass'
 
 export const Countdown = () => {
     return (
-        <div className={styles.container}>
-            <div>
-                <span>2</span>
-                <span>5</span>
+        <>
+            <div className={styles.container}>
+                <div>
+                    <span>2</span>
+                    <span>5</span>
+                </div>
+                <span>:</span>
+                <div>
+                    <span>0</span>
+                    <span>0</span>
+                </div>
             </div>
-            <span>:</span>
-            <div>
-                <span>0</span>
-                <span>0</span>
-            </div>
-        </div>
+            <button type="button" className={styles.startButton}>
+                Iniciar um ciclo
+            </button>
+        </>
+
     )
 }

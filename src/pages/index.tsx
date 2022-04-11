652b7c17c0ef4a2523fa4d431c191d8335f5f1df
Lucas Bandeira <lucasbandeira.mj@outlook.com>
2022-04-11 11:55:20 -0300

    Added ExperienceBar component and app styles

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from 'next'
+
+import { ExperienceBar } from '../components/ExperienceBar'
+
+import styles from '../styles/Home.module.sass'
+
+const Home: NextPage = () => {
+  return (
+    <div className={styles.container}>
+    <ExperienceBar />
+  </div>
+  )
+}
+
+export default Home

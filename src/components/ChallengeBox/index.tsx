042b2d64ea0066644ad12e6d1b47838b8222b7e4
Lucas Bandeira <lucasbandeira.mj@outlook.com>
2022-04-11 17:23:22 -0300

    Finished ChallengeBox component styling

--- src/components/ChallengeBox/index.tsx
+++ src/components/ChallengeBox/index.tsx
@@ -1,15 +1,47 @@
+import { useState } from 'react'
 import styles from './styles.module.sass'
 
 export const ChallengeBox = () => {
+    const [hasActiveChallenge, setHasActiveChallenge] = useState(true) 
+
     return (
         <div className={styles.container}>
-            <div className={styles.challengeNotActive}>
+            { hasActiveChallenge ? (
+                <div className={styles.challengeActive}>
+                    <header>Ganhe 400 xp</header>
+                    <main>
+                        <img src="icons/body.svg" alt="Body" />
+                        <strong>Novo desafio</strong>
+                        <p>
+                            Levante e faça uma caminhada de 3 minutos.
+                        </p>
+                    </main>
+                    <footer>
+                            <button 
+                                type="button"
+                                className={styles.challengeFailedButton}
+                            >
+                                Falhei
+                            </button>
+
+                            <button 
+                                type="button"
+                                className={styles.challengeSucceededButton}
+                            >
+                                Completei
+                            </button>
+                        </footer>
+                </div>
+            ) : (
+                <div className={styles.challengeNotActive}>
                 <strong>Finalize um ciclo para receber um desafio</strong>
                 <p>
                     <img src="icons/level-up.svg" alt="Level up" />
                     Avance de level completando desafios.
                 </p>
             </div>
+            )
+            }
         </div>
     )
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,11 +5,10 @@ export default class MyDocument extends Document {
         return (
             <Html>
                 <Head>
-                <link rel="preconnect" href="https://fonts.googleapis.com" />
-                <link rel="preconnect" href="https://fonts.gstatic.com" />
-                <link href="https://fonts.googleapis.com/css2?family=Inter:wght@500;600&family=Rajdhani:wght@600&display=swap" rel="stylesheet" />
-                <link rel="shortcut icon" href="favicon.png" type="image/png" />
-                <title>Move It</title>
+                    <link rel="preconnect" href="https://fonts.googleapis.com" />
+                    <link rel="preconnect" href="https://fonts.gstatic.com" />
+                    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@500;600&family=Rajdhani:wght@600&display=swap" rel="stylesheet" />
+                    <link rel="shortcut icon" href="favicon.png" type="image/png" />
                 </Head>
                 <body>
                     <Main />
